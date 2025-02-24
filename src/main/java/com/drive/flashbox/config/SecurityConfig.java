933935301d493ccba18b44de040cd750c842f761
renesuno <renesuno@gmail.com>
2025-02-25 08:56:41 +0900

    a

--- src/main/java/com/drive/flashbox/config/SecurityConfig.java
+++ src/main/java/com/drive/flashbox/config/SecurityConfig.java
@@ -1,56 +1,56 @@
-package com.drive.flashbox.config;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import com.drive.flashbox.service.UserService;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http
-    ) throws Exception {
-
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
-                        .permitAll()
-                        .requestMatchers("/posts","/signup")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-//                .formLogin(withDefaults())
-                .logout(logout -> logout.logoutSuccessUrl("/"));
-
-        return http.build();
-    }
-
-    // 인증된 데이터를 가져오는 로직(email, password 비교)
+//package com.drive.flashbox.config;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//import com.drive.flashbox.service.UserService;
+//import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//
+//@Configuration
+//public class SecurityConfig {
+//
 //    @Bean
-//    public UserDetailsService userDetailsService(UserService userService) {
+//    public SecurityFilterChain securityFilterChain(
+//            HttpSecurity http
+//    ) throws Exception {
 //
-//        return email -> userService.searchUser(email)
-//                .map(BoardDetails::from)
-//                .orElseThrow();
+//        http
+//                .csrf(csrf -> csrf.disable())
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+//                        .permitAll()
+//                        .requestMatchers("/posts","/signup")
+//                        .permitAll()
+//                        .anyRequest().authenticated()
+//                )
+////                .formLogin(withDefaults())
+//                .logout(logout -> logout.logoutSuccessUrl("/"));
 //
+//        return http.build();
 //    }
-
-    // 현재 db에는 암호화 x -> 이를 그대로 사용하기 위한 encoder 설정
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-}
-
+//
+//    // 인증된 데이터를 가져오는 로직(email, password 비교)
+////    @Bean
+////    public UserDetailsService userDetailsService(UserService userService) {
+////
+////        return email -> userService.searchUser(email)
+////                .map(BoardDetails::from)
+////                .orElseThrow();
+////
+////    }
+//
+//    // 현재 db에는 암호화 x -> 이를 그대로 사용하기 위한 encoder 설정
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+//
+//}
+//
--- src/main/java/com/drive/flashbox/entity/BoxUser.java
+++ src/main/java/com/drive/flashbox/entity/BoxUser.java
@@ -39,7 +39,7 @@ public class BoxUser {
     @Column(name = "role")
     private RoleType role;
 
-    @Builder
+    
     public BoxUser(Long id,
                    User user,
                    Box box,
