d3bd123d3de37e76042388e92724efa1309a11fd
leGit-y <99312658+leGit-y@users.noreply.github.com>
2025-02-27 13:50:23 +0900

    feat: dto 추가

--- src/main/java/com/drive/flashbox/dto/response/RefreshTokenResponse.java
+++ src/main/java/com/drive/flashbox/dto/response/RefreshTokenResponse.java
@@ -1,2 +1,15 @@
-package com.drive.flashbox.dto.response;public class RefreshTokenResponse {
+package com.drive.flashbox.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+public class RefreshTokenResponse {
+    Long uid;
+    String name;
+    String accessToken;
+    String refreshToken;
 }
